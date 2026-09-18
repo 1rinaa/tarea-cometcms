10ffbc32ed738fb2e2fdb2a37a62a9d8aee3e3ee
1rinaa <irina.rivera.chavarria@est.una.ac.cr>
2026-09-17 19:42:50 -0600

    ef

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    preset: 'netlify'
+    preset: 'vercel' // Asegura la compatibilidad con Vercel
   },
   
   compatibilityDate: '2026-06-30',
